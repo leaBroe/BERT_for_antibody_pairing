4ebc766837818eed69836331e209014ec8d8faf6
leaBroe <lea.broennimann@students.unibe.ch>
2024-03-31 13:15:30 +0200

    added sh file for testing gpu

---
+++ light_model/src/run_test_gpu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=48G
+#SBATCH --gres=gpu:a100:1
+#SBATCH --job-name=run_light_model
+#SBATCH --output=/ibmm_data2/oas_database/paired_lea_tmp/light_model/outputs/transformers_mlm/test_gpu_%j.o
+#SBATCH --error=/ibmm_data2/oas_database/paired_lea_tmp/light_model/outputs/transformers_mlm/test_gpu%j.e
+
+eval "$(conda shell.bash hook)"
+conda init bash
+conda activate transformers_mlm
+/home/leab/anaconda3/envs/transformers_mlm/bin/python /ibmm_data2/oas_database/paired_lea_tmp/light_model/src/test_gpu.py
