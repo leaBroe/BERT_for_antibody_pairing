9a3ddb2e042529535349d5b016f81b62f02dfe84
leaBroe <lea.broennimann@students.unibe.ch>
2024-04-02 12:16:50 +0200

    fixed small typo

--- paired_model/src/create_fasta_csv_paired_model.sh
+++ paired_model/src/create_fasta_csv_paired_model.sh
@@ -8,8 +8,8 @@
 #SBATCH --error=/ibmm_data2/oas_database/paired_lea_tmp/paired_model/logs/paired_create_fasta%j.e
 
 # create a new column in the database that contains the full sequence alignment (heavy + light)
-sqlite3 /ibmm_data2/oas_database/OAS_paired.db "ALTER TABLE all_human_paired ADD COLUMN sequence_alignment_aa_full;"
-sqlite3 /ibmm_data2/oas_database/OAS_paired.db "UPDATE all_human_paired SET sequence_alignment_aa_full = sequence_alignment_aa_heavy || sequence_alignment_aa_light;"
+#sqlite3 /ibmm_data2/oas_database/OAS_paired.db "ALTER TABLE all_human_paired ADD COLUMN sequence_alignment_aa_full TEXT;"
+#sqlite3 /ibmm_data2/oas_database/OAS_paired.db "UPDATE all_human_paired SET sequence_alignment_aa_full = sequence_alignment_aa_heavy || sequence_alignment_aa_light;"
 
 ## extract rowid and cdrh3 in csv and fasta format (for the clustering)
 sqlite3 -header -csv /ibmm_data2/oas_database/OAS_paired.db "SELECT ROWID, cdr3_aa_heavy, sequence_alignment_aa_heavy, sequence_alignment_aa_light, sequence_alignment_aa_full FROM all_human_paired;" | tee >(awk -F, '{print ">"$1"\n"$2}' > /ibmm_data2/oas_database/paired_lea_tmp/paired_model/data_oas/paired_rowid_cdrh3.fasta) > /ibmm_data2/oas_database/paired_lea_tmp/paired_model/data_oas/paired_rowid_cdrh3_full.csv
