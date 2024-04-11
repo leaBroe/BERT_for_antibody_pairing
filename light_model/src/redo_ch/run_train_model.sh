97fbd1a204c7b7bda32031a6e796bb3fbfba8b77
leaBroe <lea.broennimann@students.unibe.ch>
2024-04-11 10:40:09 +0200

    updated scripts for MLM

--- light_model/src/redo_ch/run_train_model.sh
+++ light_model/src/redo_ch/run_train_model.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#SBATCH --gres=gpu:h100:1
-#SBATCH --job-name=light_model
+
+#SBATCH --gres=gpu:a100:1
+#SBATCH --job-name=full_light_model
 
 #--config_name 'config3.json' \
 
 python run_mlm.py \
     --model_type 'roberta' \
     --tokenizer_name ./ProteinTokenizer \
-    --train_file /ibmm_data2/oas_database/paired_lea_tmp/light_model/data/training_set_test.txt \
-    --validation_file /ibmm_data2/oas_database/paired_lea_tmp/light_model/data/test_set_test.txt \
+    --train_file /ibmm_data2/oas_database/paired_lea_tmp/light_model/data/train_test_val_datasets/light_all_seqs_train_no_ids.txt \
+    --validation_file /ibmm_data2/oas_database/paired_lea_tmp/light_model/data/train_test_val_datasets/light_all_seqs_val_no_ids.txt \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 16 \
     --do_train \
@@ -24,7 +25,7 @@ python run_mlm.py \
     --seed 42 \
     --data_seed 42 \
     --bf16 False \
-    --project_name light_model_unpaired \
+    --project_name light_model_unpaired_all_seqs \
     --load_best_model_at_end True \
     --metric_for_best_model 'loss' \
     --line_by_line \
