4b7fa14a373ee3ae920bd4f5099570fa69e4461f
leaBroe <lea.broennimann@students.unibe.ch>
2024-06-24 12:34:09 +0200

    updated protbert nsp

--- paired_model/protBERT/run_train_mlm_nsp_prot_bert.sh
+++ paired_model/protBERT/run_train_mlm_nsp_prot_bert.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-#SBATCH --gres=gpu:h100:1
+#SBATCH --gres=gpu:a100:1
 #SBATCH --job-name=protbert_nsp
-#SBATCH --output=/ibmm_data2/oas_database/paired_lea_tmp/paired_model/protBERT/logs/protbert_nsp_%j.o
-#SBATCH --error=/ibmm_data2/oas_database/paired_lea_tmp/paired_model/protBERT/logs/protbert_nsp_%j.e
+#SBATCH --output=/ibmm_data2/oas_database/paired_lea_tmp/paired_model/protBERT/logs/full_data_10_epochs_new_metrics_lr2e-6_batch_size_8_%j.o
+#SBATCH --error=/ibmm_data2/oas_database/paired_lea_tmp/paired_model/protBERT/logs/full_data_10_epochs_new_metrics_lr2e-6_batch_size_8_%j.e
 
 eval "$(conda shell.bash hook)"
 conda init bash
--- paired_model/protBERT/train_mlm_nsp_prot_bert.py
+++ paired_model/protBERT/train_mlm_nsp_prot_bert.py
@@ -65,19 +65,21 @@ print(f"Tokenizer's vocab_size: {tokenizer.vocab_size}")
 print("Model's vocab size from embeddings:", model.bert.embeddings.word_embeddings.num_embeddings)
 
 # Initialize wandb
-run_name = "debug_run_1"
+run_name = "full_data_10_epochs_new_metrics_lr2e-6_batch_size_8"
 
 wandb.init(project="paired_model_nsp_mlm_protbert", name=run_name)
 
 output_dir = f"./{run_name}"
 logging_dir = f"./{run_name}_logging"
 
+batch_size=8
+
 # define the arguments for the trainer
 training_args = TrainingArguments(
     output_dir=output_dir,          # output directory
-    num_train_epochs=5,              # total # of training epochs
-    per_device_train_batch_size=16,  # batch size per device during training (try 16 if needed)
-    per_device_eval_batch_size=16,   # batch size for evaluation
+    num_train_epochs=10,              # total # of training epochs
+    per_device_train_batch_size=batch_size,  # batch size per device during training (try 16 if needed)
+    per_device_eval_batch_size=batch_size,   # batch size for evaluation
     warmup_steps=500,                # number of warmup steps for learning rate scheduler
     weight_decay=0.01,               # strength of weight decay
     logging_dir=logging_dir,     # directory for storing logs
@@ -86,25 +88,25 @@ training_args = TrainingArguments(
     logging_steps=5
 )
  
-# Debugging: Check if all input_ids are within the tokenizer's vocabulary size
-def check_input_ids_validity(dataset, tokenizer):
-    vocab_size = tokenizer.vocab_size
-    for example in dataset:
-        # Extracting input_ids from each example
-        input_ids = example['input_ids'] if isinstance(example, dict) else example.input_ids
-        max_id = max(input_ids)
-        if max_id >= vocab_size:
-            raise ValueError(f"An input_id ({max_id}) exceeds the tokenizer's vocabulary size ({vocab_size}).")
-    print(f"All input_ids are within the vocabulary size.")
+# # Debugging: Check if all input_ids are within the tokenizer's vocabulary size
+# def check_input_ids_validity(dataset, tokenizer):
+#     vocab_size = tokenizer.vocab_size
+#     for example in dataset:
+#         # Extracting input_ids from each example
+#         input_ids = example['input_ids'] if isinstance(example, dict) else example.input_ids
+#         max_id = max(input_ids)
+#         if max_id >= vocab_size:
+#             raise ValueError(f"An input_id ({max_id}) exceeds the tokenizer's vocabulary size ({vocab_size}).")
+#     print(f"All input_ids are within the vocabulary size.")
 
 
-# Debugging: Log input_ids right before training to ensure they are all valid
-def log_input_ids(data_loader):
-    for batch in data_loader:
-        input_ids = batch['input_ids']
-        if torch.any(input_ids >= tokenizer.vocab_size):
-            print("Invalid input_ids detected:", input_ids)
-        assert torch.all(input_ids < tokenizer.vocab_size), f"Found input_ids >= vocab size: {input_ids.max().item()}"
+# # Debugging: Log input_ids right before training to ensure they are all valid
+# def log_input_ids(data_loader):
+#     for batch in data_loader:
+#         input_ids = batch['input_ids']
+#         if torch.any(input_ids >= tokenizer.vocab_size):
+#             print("Invalid input_ids detected:", input_ids)
+#         assert torch.all(input_ids < tokenizer.vocab_size), f"Found input_ids >= vocab size: {input_ids.max().item()}"
 
 
 # small dataset with input heavyseq[SEP]lightseq with each AA SPACE SEPARATED!!
@@ -119,25 +121,25 @@ full_val_dataset_path = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/tr
 print("start building train_dataset=", datetime.now(PST))
 train_dataset = TextDatasetForNextSentencePrediction(
     tokenizer=tokenizer,
-    file_path=small_train_dataset_path,
+    file_path=full_train_dataset_path,
     block_size=512
 )
 
 # Check train_dataset input_ids
-print("Checking train_dataset input_ids...")
-check_input_ids_validity(train_dataset, tokenizer)
+#print("Checking train_dataset input_ids...")
+#check_input_ids_validity(train_dataset, tokenizer)
 
 # Prepare the eval_dataset
 print("start building eval_dataset=", datetime.now(PST))
 eval_dataset = TextDatasetForNextSentencePrediction(
     tokenizer=tokenizer,
-    file_path=small_val_dataset_path,
+    file_path=full_val_dataset_path,
     block_size=512
 )
 
 # Check eval_dataset input_ids
-print("Checking eval_dataset input_ids...")
-check_input_ids_validity(eval_dataset, tokenizer)
+#print("Checking eval_dataset input_ids...")
+#check_input_ids_validity(eval_dataset, tokenizer)
 
 # Initialize the Data Collator
 data_collator = DataCollatorForLanguageModeling(
@@ -145,31 +147,32 @@ data_collator = DataCollatorForLanguageModeling(
 )
 
 # Function to create a DataLoader with debugging -> to see the first batch before and after data collator, see if it handles the data correctly
-def create_data_loader_with_debugging(dataset, batch_size, data_collator):
-    data_loader = DataLoader(dataset, batch_size=batch_size, collate_fn=data_collator)
-    for batch in data_loader:
-        input_ids = batch['input_ids']
-        attention_mask = batch['attention_mask']
-        labels = batch['labels']
-        next_sentence_label = batch['next_sentence_label']
+# def create_data_loader_with_debugging(dataset, batch_size, data_collator):
+#     data_loader = DataLoader(dataset, batch_size=batch_size, collate_fn=data_collator)
+#     for batch in data_loader:
+#         input_ids = batch['input_ids']
+#         attention_mask = batch['attention_mask']
+#         labels = batch['labels']
+#         next_sentence_label = batch['next_sentence_label']
         
-        # Print the first batch to debug
-        print("First batch before data collator:")
-        for i, example in enumerate(dataset):
-            if i == batch_size:
-                break
-            print(f"Example {i}: {tokenizer.decode(example['input_ids'])}")
+#         # Print the first batch to debug
+#         print("First batch before data collator:")
+#         for i, example in enumerate(dataset):
+#             if i == batch_size:
+#                 break
+#             print(f"Example {i}: {tokenizer.decode(example['input_ids'])}")
         
-        print("First batch after data collator:")
-        print(f"Input IDs: {input_ids}")
-        print(f"Attention Mask: {attention_mask}")
-        print(f"Labels: {labels}")
-        print(f"Next Sentence Labels: {next_sentence_label}")
-    return data_loader
+#         print("First batch after data collator:")
+#         print(f"Input IDs: {input_ids}")
+#         print(f"Attention Mask: {attention_mask}")
+#         print(f"Labels: {labels}")
+#         print(f"Next Sentence Labels: {next_sentence_label}")
+#         break
+#     return data_loader
 
 
-# Create and debug the DataLoader
-train_data_loader = create_data_loader_with_debugging(train_dataset, batch_size=16, data_collator=data_collator)
+# # Create and debug the DataLoader
+# train_data_loader = create_data_loader_with_debugging(train_dataset, batch_size=16, data_collator=data_collator)
 
 
 # def compute_metrics(preds, labels):
@@ -299,48 +302,48 @@ trainer = CustomTrainer(
 print("finished=",datetime.now(PST))
 
 # Verifying all input_ids in DataLoader before training
-print("Verifying all input_ids in DataLoader before training...")
-train_data_loader = DataLoader(train_dataset, batch_size=16, collate_fn=data_collator)
-log_input_ids(train_data_loader)
+#print("Verifying all input_ids in DataLoader before training...")
 
-learning_rate = 2e-5
+#log_input_ids(train_data_loader)
+
+learning_rate = 2e-6
 
 optimizer = AdamW(model.parameters(), lr=learning_rate)
 
 # Create DataLoader for debugging
-train_data_loader = DataLoader(train_dataset, batch_size=16, collate_fn=data_collator)
-eval_data_loader = DataLoader(eval_dataset, batch_size=16, collate_fn=data_collator)
+train_data_loader = DataLoader(train_dataset, batch_size=batch_size, collate_fn=data_collator)
+eval_data_loader = DataLoader(eval_dataset, batch_size=batch_size, collate_fn=data_collator)
 
 
-print("Verifying all input_ids in DataLoader before training...")
-log_input_ids(train_data_loader)  # This will raise an error if any input_id is out of range
+#print("Verifying all input_ids in DataLoader before training...")
+#log_input_ids(train_data_loader)  # This will raise an error if any input_id is out of range
 
-# Testing DataLoader outputs
-for batch in train_data_loader:
-    input_ids = batch['input_ids']
-    attention_mask = batch['attention_mask']
-    print(f"Batch input_ids shape: {input_ids.shape}")  # Should show consistent shapes within a batch
-    print(f"Batch attention mask shape: {attention_mask.shape}")  # Verify attention masks are correct
+# # Testing DataLoader outputs
+# for batch in train_data_loader:
+#     input_ids = batch['input_ids']
+#     attention_mask = batch['attention_mask']
+#     print(f"Batch input_ids shape: {input_ids.shape}")  # Should show consistent shapes within a batch
+#     print(f"Batch attention mask shape: {attention_mask.shape}")  # Verify attention masks are correct
 
-    # Check if any input_id in the batch exceeds the tokenizer's vocab size
-    if torch.any(input_ids >= tokenizer.vocab_size):
-        print("Invalid input_ids detected:", input_ids)
-        break
+#     # Check if any input_id in the batch exceeds the tokenizer's vocab size
+#     if torch.any(input_ids >= tokenizer.vocab_size):
+#         print("Invalid input_ids detected:", input_ids)
+#         break
 
 
 # Example to check how the tokenizer handles one of your sequences
-sample_sequence = "Q V Q L Q E S G P G L V K P S E T L S L T C T V S G G S I S G F Y W S W I R Q S P G K G L E"
-tokens = tokenizer.tokenize(sample_sequence)
-token_ids = tokenizer.convert_tokens_to_ids(tokens)
+# sample_sequence = "Q V Q L Q E S G P G L V K P S E T L S L T C T V S G G S I S G F Y W S W I R Q S P G K G L E"
+# tokens = tokenizer.tokenize(sample_sequence)
+# token_ids = tokenizer.convert_tokens_to_ids(tokens)
 
-print("Tokens:", tokens)
-print("Token IDs:", token_ids)
+# print("Tokens:", tokens)
+# print("Token IDs:", token_ids)
 
 # turn txt fiel with sequences into a list of sequences
-def read_txt_file(file_path):
-    with open(file_path, 'r') as f:
-        lines = f.readlines()
-    return [line.strip() for line in lines]
+# def read_txt_file(file_path):
+#     with open(file_path, 'r') as f:
+#         lines = f.readlines()
+#     return [line.strip() for line in lines]
 
 # # Load the sequences from the txt file
 # train_data = read_txt_file(small_train_dataset_path)
@@ -352,31 +355,34 @@ def read_txt_file(file_path):
 # print("Unknown Characters:", unknown_chars)
 
 # verify token type ids
-def verify_token_type_ids(data_loader):
-    for i, batch in enumerate(data_loader):
-        input_ids = batch['input_ids']
-        token_type_ids = batch.get('token_type_ids', torch.zeros_like(input_ids))  # Assuming default of all zeros if not provided
+# def verify_token_type_ids(data_loader):
+#     for i, batch in enumerate(data_loader):
+#         input_ids = batch['input_ids']
+#         token_type_ids = batch.get('token_type_ids', torch.zeros_like(input_ids))  # Assuming default of all zeros if not provided
         
-        # Printing unique token type ids to verify them
-        print(f"Batch {i}: Unique token type ids:", torch.unique(token_type_ids))
+#         # Printing unique token type ids to verify them
+#         print(f"Batch {i}: Unique token type ids:", torch.unique(token_type_ids))
 
-        # Optionally check max input id
-        if input_ids.max().item() >= tokenizer.vocab_size:
-            print("Error: input_ids exceed vocab size")
-            break
+#         # Optionally check max input id
+#         if input_ids.max().item() >= tokenizer.vocab_size:
+#             print("Error: input_ids exceed vocab size")
+#             break
 
-        # Print a message if everything seems fine
-        print(f"Batch {i}: All token type IDs are within expected range.")
+#         # Print a message if everything seems fine
+#         print(f"Batch {i}: All token type IDs are within expected range.")
         
-        # Breaking after a few batches for demonstration
-        if i > 5:  # Check first 5 batches
-            break
+#         # Breaking after a few batches for demonstration
+#         if i > 5:  # Check first 5 batches
+#             break
 
 # Run this function to verify token type ids in your DataLoader
-verify_token_type_ids(train_data_loader)
+#verify_token_type_ids(train_data_loader)
 
 # Prepare the learning rate scheduler
 num_training_steps = len(train_data_loader) * training_args.num_train_epochs
+
+print(f"Number of training steps: {num_training_steps}")
+
 lr_scheduler = get_scheduler(
     "linear",
     optimizer=optimizer,
@@ -392,12 +398,12 @@ model.to(device)
 os.makedirs(training_args.output_dir, exist_ok=True)
 os.makedirs(training_args.logging_dir, exist_ok=True)
 
-def find_nan(tensor):
-    nan_mask = torch.isnan(tensor)
-    if torch.any(nan_mask):
-        first_nan_idx = torch.where(nan_mask)[0][0].item()
-        return first_nan_idx
-    return None
+# def find_nan(tensor):
+#     nan_mask = torch.isnan(tensor)
+#     if torch.any(nan_mask):
+#         first_nan_idx = torch.where(nan_mask)[0][0].item()
+#         return first_nan_idx
+#     return None
 
 
 # # Custom model for debugging -> to check for NaNs in the model outputs
@@ -537,7 +543,7 @@ for epoch in range(training_args.num_train_epochs):
             all_preds.extend(mlm_preds.cpu().numpy())
             all_labels.extend(batch['labels'].cpu().numpy())
 
-            print(f"all_labels {all_labels}")
+            #print(f"all_labels {all_labels}")
 
             # we still need softmax to convert the logits into probabilities
             # index 0: sequence B is a continuation of sequence A
