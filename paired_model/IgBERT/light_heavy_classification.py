c7a67ae4bcf92814208fc0491ef4fb70f7b286f1
leaBroe <lea.broennimann@students.unibe.ch>
2024-07-11 13:59:08 +0200

    updated classification

--- paired_model/IgBERT/light_heavy_classification.py
+++ paired_model/IgBERT/light_heavy_classification.py
@@ -11,9 +11,23 @@ import logging
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, classification_report
 import wandb
 
+########################################################################################################################################################################################################################
+# sequence classification with own classifier
+########################################################################################################################################################################################################################
+
+
+# Set up parameters
+bert_model_name = 'Exscientia/IgBERT'
+#bert_model_name = 'Rostlab/prot_bert_bfd'
+num_classes = 2
+max_length = 512
+batch_size = 16
+num_epochs = 50
+learning_rate = 2e-5
+
 # initialize Weights & Biases
 
-wandb.init(project='paired_classification_heavy_light', name='igbert_full_1e-2')
+wandb.init(project='classification_heavy_light_own_classifier', name=f'igbert_full_lr_{learning_rate}_batch_{batch_size}_epochs_{num_epochs}')
 
 logging.basicConfig(level=logging.INFO)
 
@@ -23,12 +37,12 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # print device
 print(f"Device: {device}")
 
-# the input data is a csv file with columns 'heavy', 'light', 'label'
+# the input data is a csv file with columns 'heavy', 'light', 'label' and single space separated AAs
 # 1 for paired, 0 for not paired
 # example:
 # heavy,light,label
-# GLEWIAYIYFSGSTNYNPSLKSRVTLSVDTSKNQFSLKLSSVTAADSAVYYCARDVGPYNSISPGRYYFDYWGPGTLVTVSS,QSALTQPASVSGSPGQSITISCTGTSSDVGNYNLVSWYQHHPGKAPKLMIYEVSKRPSGISNRFSGSKSGNTASLTISGLQADDEADYYCCSYAGSRILYVFGSGTKVTVL,1
-# QLQLQESGPGLVKPSETLSLTCTVSGGSISSSSYYWGWIRQPPGKGLEWIGNFFYSGSTNYNPSLKSRATISLDTSKNELSLKLSSVTAADTAVYYCASNTLMAEATFDYWGQGTLVTVSS,SYEVTQAPSVSVSPGQTASVTCSGDKLDKKYTSWYQQRPGQSPTVVIYQNNKRPSGIPERFSASKSGNTATLTISGTQAVDEADYYCQAWDDSDGVFGPGTTVTVL,0
+# E V Q L V E S G G G L V Q P G G S L R L S C A A S G F T F S S Y D M H W V R Q A T G K G L E W V S A I G T A G D T Y Y P G S G K G R F T I S R E N A K N S L Y L Q M N S L R A G D T A V Y Y C A R A R P V G Y C S G G L G C G A F D I W G Q G T M V T V S S , S Y E L T Q P P S V S V S P G Q T A R I T C S G D A L P K Q Y A Y W Y Q H K P G Q A P V L V I Y K D S E R P S G I P E R F S G S S S G T T V T L T I S G V Q A E D E A D Y Y C Q S A D S S G T Y V V F G G G T K L T V L ,1
+# Q V Q L Q E S G P G L V K P S E T L S L T C A V S G Y S I S S G Y Y W G W I R Q P P G K G L E W I G S I Y H S G S T Y Y N P S L K S R V T I S V D T S K N Q F S L K L S S V T A A D T A V Y Y C A R Y C G G D C Y Y V P D Y W G Q G T L V T V S S , S Y E L T Q P P S V S V S P G Q T A S I T C S G D K L G D K Y A C W Y Q Q K P G Q S P V L V I Y Q D S K R P S G I P E R F S G S N S G N T A T L T I S G T Q A M D E A D Y Y C Q A W D S S T E V V F G G G T K L T V L ,0
 
 def load_paired_data(data_file):
     df = pd.read_csv(data_file)
@@ -180,21 +194,12 @@ def predict_pairing(heavy, light, model, tokenizer, device, max_length=512):
     return "paired" if preds.item() == 1 else "not paired"
 
 
-# Set up parameters
-bert_model_name = 'Exscientia/IgBERT'
-#bert_model_name = 'Rostlab/prot_bert_bfd'
-num_classes = 2
-max_length = 512
-batch_size = 16
-num_epochs = 11
-learning_rate = 2e-5
-
 # Load the training and validation data from separate files
 #train_file = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/IgBERT/paired_full_seqs_sep_train_with_unpaired.csv"
 #val_file = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/IgBERT/paired_full_seqs_sep_val_with_unpaired.csv"
 
-train_file = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/IgBERT/paired_full_seqs_sep_train_with_unpaired_small.csv"
-val_file = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/IgBERT/paired_full_seqs_sep_val_with_unpaired_small.csv"
+train_file = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/IgBERT/paired_full_seqs_sep_train_with_unpaired_small_space_separated_rm.csv"
+val_file = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/IgBERT/paired_full_seqs_sep_val_with_unpaired_small_space_separated_rm.csv"
 
 train_heavy, train_light, train_labels = load_paired_data(train_file)
 val_heavy, val_light, val_labels = load_paired_data(val_file)
