d455b5c3e45b316e90b81c326e6f889ee56c2f88
leaBroe <lea.broennimann@students.unibe.ch>
2024-07-22 13:53:28 +0200

    added script for file conversion and small example data

---
+++ paired_model/BERT2BERT/sqlite3_data_for_analysis/convert_to_fasta.jl
@@ -0,0 +1,21 @@
+using CSV
+using DataFrames
+
+# Read the CSV file
+input_file = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/small_test_data/small_paired_data_for_analysis.csv"  # Change this to your CSV file name
+df = CSV.read(input_file, DataFrame)
+
+# Open the FASTA file for writing
+output_file = open("/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/small_test_data/small_paired_data_sequence_alignment_light.fasta", "w")
+
+# Iterate over each row and write to the FASTA file
+for (i, row) in enumerate(eachrow(df))
+    sequence = row[:sequence_alignment_light]
+    if !ismissing(sequence)
+        println(output_file, ">$(i)")
+        println(output_file, sequence)
+    end
+end
+
+# Close the FASTA file
+close(output_file)
