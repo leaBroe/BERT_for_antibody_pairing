c18bc0d0fda3834d73c77c9e9dbb9ef341345bdb
leaBroe <lea.broennimann@students.unibe.ch>
2024-07-26 12:41:57 +0200

    added kappa lambda results and notebook

--- paired_model/BERT2BERT/sqlite3_data_for_analysis/kappa_lambda_analysis/kappa_lambda_subtypes_analysis.py
+++ paired_model/BERT2BERT/sqlite3_data_for_analysis/kappa_lambda_analysis/kappa_lambda_subtypes_analysis.py
@@ -35,9 +35,13 @@ if seq_pair:
 # Convert to DataFrame
 df1 = pd.DataFrame(data1)
 
+print(len(df1))
+
 # Read the fourth file
 df4 = pd.read_csv(file4_path)
 df4['sequence_alignment_aa'] = df4['sequence_alignment_aa'].str.replace(' ', '')
+
+print(len(df4))
 
 # Read the additional file
 df_additional = pd.read_csv(additional_file_path)
@@ -45,32 +49,40 @@ df_additional = pd.read_csv(additional_file_path)
 # Extract the light sequence after the [SEP] token and remove spaces
 df_additional['Light Sequence'] = df_additional['sequence_alignment_heavy_sep_light'].apply(lambda x: x.split('[SEP]')[-1].replace(' ', ''))
 
+print(len(df_additional))
+
 # Merge df1 and df4 on True Sequence and sequence_alignment_aa
-merged_df = pd.merge(df1, df4, left_on='True Sequence', right_on='sequence_alignment_aa', how='inner')
+merged_df = pd.merge(df1, df_additional, left_on='True Sequence', right_on='Light Sequence', how='inner')
 
-# Merge with df_additional on True Sequence and Light Sequence
-final_merged_df = pd.merge(merged_df, df_additional, left_on='True Sequence', right_on='Light Sequence', how='inner')
+print(len(merged_df))
 
-final_merged_df.drop_duplicates(subset=['sequence_alignment_heavy_sep_light'])
+merged_df.drop_duplicates(subset=['sequence_alignment_heavy_sep_light'])
 
-print(len(final_merged_df))
+print(len(merged_df))   
 
-# Select final columns and rename for clarity
-final_df = final_merged_df[['True Sequence', 'Generated Sequence', 'BLOSUM Score', 'Similarity Percentage', 'Perplexity', 'v_family', 'sequence_alignment_heavy_sep_light']]
-final_df = final_df.rename(columns={
-    'True Sequence': 'True Sequence',
-    'Generated Sequence': 'Generated Sequence',
-    'BLOSUM Score': 'BLOSUM Score',
-    'Similarity Percentage': 'Similarity Percentage',
-    'Perplexity': 'Perplexity',
-    'v_family': 'Subtype',
-    'sequence_alignment_heavy_sep_light': 'sequence_alignment_heavy_sep_light'
-})
+# # Merge with df_additional on True Sequence and Light Sequence
+# final_merged_df = pd.merge(merged_df, df_additional, left_on='True Sequence', right_on='Light Sequence', how='inner')
 
-# Save the final DataFrame
-output_csv_path = '/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/kappa_lambda_analysis/kappa_lambda_subtypes_analysis_output.csv'
-final_df.to_csv(output_csv_path, index=False)
+# final_merged_df.drop_duplicates(subset=['sequence_alignment_heavy_sep_light'])
 
-# Display the final DataFrame (for verification)
-final_df.head()
+# print(len(final_merged_df))
+
+# # Select final columns and rename for clarity
+# final_df = final_merged_df[['True Sequence', 'Generated Sequence', 'BLOSUM Score', 'Similarity Percentage', 'Perplexity', 'v_family', 'sequence_alignment_heavy_sep_light']]
+# final_df = final_df.rename(columns={
+#     'True Sequence': 'True Sequence',
+#     'Generated Sequence': 'Generated Sequence',
+#     'BLOSUM Score': 'BLOSUM Score',
+#     'Similarity Percentage': 'Similarity Percentage',
+#     'Perplexity': 'Perplexity',
+#     'v_family': 'Subtype',
+#     'sequence_alignment_heavy_sep_light': 'sequence_alignment_heavy_sep_light'
+# })
+
+# # Save the final DataFrame
+# output_csv_path = '/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/kappa_lambda_analysis/kappa_lambda_subtypes_analysis_output.csv'
+# final_df.to_csv(output_csv_path, index=False)
+
+# # Display the final DataFrame (for verification)
+# final_df.head()
 
