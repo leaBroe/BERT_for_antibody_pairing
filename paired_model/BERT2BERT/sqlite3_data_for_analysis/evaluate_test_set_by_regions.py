9c24b9ff47ca224ffa5e63150cb3fae85394ca61
leaBroe <lea.broennimann@students.unibe.ch>
2024-08-13 10:08:58 +0200

    updated regions analysis scripts

--- paired_model/BERT2BERT/sqlite3_data_for_analysis/evaluate_test_set_by_regions.py
+++ paired_model/BERT2BERT/sqlite3_data_for_analysis/evaluate_test_set_by_regions.py
@@ -38,9 +38,14 @@ codon_table = {
     'T': 'ACT', 'V': 'GTT', 'W': 'TGG', 'Y': 'TAT', '*': 'TAA'
 }
 
+# 1. Extract sequences from the log file and convert them to DNA because PyIR requires DNA sequences and cannot handle protein sequences.
+# Here I am using a simplified codon table to reverse translate the protein sequences to DNA sequences, because there are multiple possible codons for some amino acids.
+# This is not ideal, but there is no direct way to convert protein sequences to DNA sequences without ambiguity.
+
 # Function to reverse translate protein sequence to DNA
 def protein_to_dna(protein_seq):
     return "".join([codon_table[aa] for aa in protein_seq])
+
 
 def extract_sequences(file_path):
     data = []
@@ -67,8 +72,17 @@ def extract_sequences(file_path):
 
     return data
 
-# Example usage
-# file_path = '/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/logs/full_eval_heavy2light_with_adapters125463.o' 
+# input: log file of the form:
+#
+# Sequence pair 67209:
+# True Sequence: D I Q V T Q S P S S L S A S I G D R V T I T C Q A S Q D I S D N L N W Y Q Q K P G K V P K L L I Y D A S N L Q T G V P S R F S G S G S G T Y F S V T I S S L Q P E D I A T Y Y C Q S Y G K F R P R T F G Q G T K L E I K
+# Generated Sequence: D I Q M T Q S P S S L S A S V G D R V T I T C R A S Q S I S S Y L N W Y Q Q K P G K A P K L L I Y A A S S L Q S G V P S R F S G S G S G T D F T L T I S S L Q P E D F A T Y Y C Q Q S Y S T P R T F G Q G T K V E I K
+# BLOSUM Score: 355.0
+# Similarity Percentage: 70.09345794392523%
+# Perplexity: 2.4618451595306396
+
+# #file_path = '/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/logs/full_eval_heavy2light_with_adapters125463.o' 
+# file_path = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/logs/full_eval_full_diverse_beam_search_2_temp_0.5_max_length_150_early_stopping_true_batch_size_64_epochs_10_lr_0.0001_wd_0.1_127798.o"
 # data = extract_sequences(file_path)
 
 # # Step 1 & 2: Extract sequences and convert to DNA
@@ -82,91 +96,92 @@ def extract_sequences(file_path):
 #     fasta_entries.append((f">Generated_Seq_{i+1}", generated_seq_dna))
 
 # # Step 3: Write to FASTA file
-# fasta_file = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/evaluate_test_set_by_regions/sequences.fasta"
+# # output file:
+# fasta_file = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/evaluate_test_set_by_regions/h2l_div_beam_search_2_epoch_10_lr_1e-4_wd_0.1/true_gen_sequences_in_DNA.fasta"
 # with open(fasta_file, "w") as f:
 #     for header, sequence in fasta_entries:
 #         f.write(f"{header}\n{sequence}\n")
 
-# fasta_file = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/evaluate_test_set_by_regions/sequences.fasta"
+#fasta_file = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/evaluate_test_set_by_regions/sequences.fasta"
+fasta_file="/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/evaluate_test_set_by_regions/h2l_div_beam_search_2_epoch_10_lr_1e-4_wd_0.1/true_gen_sequences_in_DNA.fasta"
+
+# Step 4: Use PyIR to identify regions 
+pyirfile = PyIR(query=fasta_file, args=['--outfmt', 'tsv'])
+pyir_result = pyirfile.run()
+
+# # Step 5: Calculate similarity and BLOSUM scores
+# blosum62 = substitution_matrices.load("BLOSUM62")
+# results = []
 
 
-# # Step 4: Use PyIR to identify regions 
-# pyirfile = PyIR(query=fasta_file, args=['--outfmt', 'tsv'])
-# pyir_result = pyirfile.run()
-
-# Step 5: Calculate similarity and BLOSUM scores
-blosum62 = substitution_matrices.load("BLOSUM62")
-results = []
-
-
-def calculate_blosum_score(true_seq, generated_seq, matrix):
-    if not true_seq or not generated_seq:
-        raise ValueError("True sequence or generated sequence is empty.")
+# def calculate_blosum_score(true_seq, generated_seq, matrix):
+#     if not true_seq or not generated_seq:
+#         raise ValueError("True sequence or generated sequence is empty.")
     
-    score = 0
-    matches = 0
-    min_length = min(len(true_seq), len(generated_seq))
+#     score = 0
+#     matches = 0
+#     min_length = min(len(true_seq), len(generated_seq))
 
-    for i in range(min_length):
-        pair = (true_seq[i], generated_seq[i])
-        if pair in matrix:
-            score += matrix[pair]
-        elif (pair[1], pair[0]) in matrix:
-            score += matrix[(pair[1], pair[0])]
-        if true_seq[i] == generated_seq[i]:
-            matches += 1
+#     for i in range(min_length):
+#         pair = (true_seq[i], generated_seq[i])
+#         if pair in matrix:
+#             score += matrix[pair]
+#         elif (pair[1], pair[0]) in matrix:
+#             score += matrix[(pair[1], pair[0])]
+#         if true_seq[i] == generated_seq[i]:
+#             matches += 1
 
-    similarity_percentage = (matches / min_length) * 100 if min_length > 0 else 0
-    return score, min_length, matches, similarity_percentage
+#     similarity_percentage = (matches / min_length) * 100 if min_length > 0 else 0
+#     return score, min_length, matches, similarity_percentage
 
 
-# Read the CSV file into a DataFrame
-df = pd.read_csv('/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/evaluate_test_set_by_regions/full_test_set_true_gen_seqs_relevant_cols.csv')
+# # Read the CSV file into a DataFrame
+# df = pd.read_csv('/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/evaluate_test_set_by_regions/full_test_set_true_gen_seqs_relevant_cols.csv')
 
-# Define regions to process
-regions = ['fwr1_aa', 'cdr1_aa', 'fwr2_aa', 'cdr2_aa', 'fwr3_aa', 'cdr3_aa', 'fwr4_aa']
+# # Define regions to process
+# regions = ['fwr1_aa', 'cdr1_aa', 'fwr2_aa', 'cdr2_aa', 'fwr3_aa', 'cdr3_aa', 'fwr4_aa']
 
-# List to store results
-results = []
+# # List to store results
+# results = []
 
-# Iterate over the DataFrame in pairs (True, Generated)
-for i in range(0, len(df), 2):
-    true_seq_row = df.iloc[i]
-    generated_seq_row = df.iloc[i + 1]
+# # Iterate over the DataFrame in pairs (True, Generated)
+# for i in range(0, len(df), 2):
+#     true_seq_row = df.iloc[i]
+#     generated_seq_row = df.iloc[i + 1]
 
-    result_entry = {'sequence_id': true_seq_row['sequence_id']}
+#     result_entry = {'sequence_id': true_seq_row['sequence_id']}
     
-    for region in regions:
-        true_seq = true_seq_row[region]
-        generated_seq = generated_seq_row[region]
+#     for region in regions:
+#         true_seq = true_seq_row[region]
+#         generated_seq = generated_seq_row[region]
 
-        # Handle missing values by setting them to empty strings
-        if pd.isna(true_seq):
-            true_seq = ""
-        if pd.isna(generated_seq):
-            generated_seq = ""
+#         # Handle missing values by setting them to empty strings
+#         if pd.isna(true_seq):
+#             true_seq = ""
+#         if pd.isna(generated_seq):
+#             generated_seq = ""
         
-        # Skip if both sequences are empty
-        if not true_seq and not generated_seq:
-            print(f"Both sequences are empty for region {region} in sequence pair {true_seq_row['sequence_id']}. Skipping.")
-            continue
+#         # Skip if both sequences are empty
+#         if not true_seq and not generated_seq:
+#             print(f"Both sequences are empty for region {region} in sequence pair {true_seq_row['sequence_id']}. Skipping.")
+#             continue
         
-        try:
-            blosum_score, min_length, matches, similarity_percentage = calculate_blosum_score(true_seq, generated_seq, blosum62)
-        except ValueError as e:
-            print(f"Error processing sequences {true_seq_row['sequence_id']} in region {region}: {e}")
-            continue
+#         try:
+#             blosum_score, min_length, matches, similarity_percentage = calculate_blosum_score(true_seq, generated_seq, blosum62)
+#         except ValueError as e:
+#             print(f"Error processing sequences {true_seq_row['sequence_id']} in region {region}: {e}")
+#             continue
         
-        result_entry[f'{region}_blosum_score'] = blosum_score
-        result_entry[f'{region}_min_length'] = min_length
-        result_entry[f'{region}_matches'] = matches
-        result_entry[f'{region}_similarity_percentage'] = similarity_percentage
+#         result_entry[f'{region}_blosum_score'] = blosum_score
+#         result_entry[f'{region}_min_length'] = min_length
+#         result_entry[f'{region}_matches'] = matches
+#         result_entry[f'{region}_similarity_percentage'] = similarity_percentage
     
-    results.append(result_entry)
+#     results.append(result_entry)
 
-# Convert results to DataFrame for analysis or export
-results_df = pd.DataFrame(results)
+# # Convert results to DataFrame for analysis or export
+# results_df = pd.DataFrame(results)
 
-# save the results to a CSV file
-results_df.to_csv('/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/evaluate_test_set_by_regions/region_similarity_scores.csv', index=False)
+# # save the results to a CSV file
+# results_df.to_csv('/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/evaluate_test_set_by_regions/region_similarity_scores.csv', index=False)
 
--- paired_model/BERT2BERT/sqlite3_data_for_analysis/extract_relevant_cols_from_tsv.py
+++ paired_model/BERT2BERT/sqlite3_data_for_analysis/extract_relevant_cols_from_tsv.py
@@ -1,3 +1,4 @@
+# used env: OAS_paired_env
 import pandas as pd
 
 def extract_columns_from_tsv(tsv_file, columns, output_csv):
@@ -21,7 +22,8 @@ def extract_columns_from_tsv(tsv_file, columns, output_csv):
 # column names PyIR: sequence_id	sequence	sequence_alignment_aa	germline_alignment_aa	fwr1	fwr1_aa	cdr1	cdr1_aa	fwr2	fwr2_aa	cdr2	cdr2_aa	fwr3	fwr3_aa	fwr4	fwr4_aa	cdr3	cdr3_aa		fwr1_start	fwr1_end	cdr1_start	cdr1_end	fwr2_start	fwr2_end	cdr2_start	cdr2_end	fwr3_start	fwr3_end	fwr4_start	fwr4_end	cdr3_start	cdr3_end	np1	np1_length	np2	np2_length	cdr3_aa_length
 
 #tsv_file = '/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/full_test_data/full_paired_data_sequence_alignment_light.tsv' 
-tsv_file = '/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/evaluate_test_set_by_regions/full_test_data_true_gen_seqs.tsv'
+#tsv_file = '/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/evaluate_test_set_by_regions/full_test_data_true_gen_seqs.tsv'
+tsv_file="/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/evaluate_test_set_by_regions/h2l_div_beam_search_2_epoch_10_lr_1e-4_wd_0.1/h2l_div_beam_search_2_epoch_10_lr_1e-4_wd_0.tsv"
 
 columns_to_extract = [
     "sequence_id",
@@ -56,13 +58,11 @@ columns_to_extract = [
     "fwr4_end",
     "cdr3_start",
     "cdr3_end",
-    "np1",
-    "np1_length",
-    "np2",
-    "np2_length",
     "cdr3_aa_length",
     "locus",
-    "v_family"
+    "v_family",
+    "j_family",
+    "d_family",
 ]
 
 # columns_to_extract = [
@@ -73,7 +73,7 @@ columns_to_extract = [
 #     "v_family"
 # ]
 
-output_csv = '/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/evaluate_test_set_by_regions/full_test_set_true_gen_seqs_all_relevant_cols.csv' 
+output_csv = '/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/evaluate_test_set_by_regions/h2l_div_beam_search_2_epoch_10_lr_1e-4_wd_0.1/full_test_set_true_gen_seqs_all_relevant_cols.csv' 
 
 extract_columns_from_tsv(tsv_file, columns_to_extract, output_csv)
 
