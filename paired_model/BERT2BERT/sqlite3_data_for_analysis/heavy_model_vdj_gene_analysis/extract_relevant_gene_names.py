f75e337b8e47e3bbb300280d908bec7ffd1f1a11
leaBroe <lea.broennimann@students.unibe.ch>
2024-09-08 21:11:39 +0200

    updated script

--- paired_model/BERT2BERT/sqlite3_data_for_analysis/heavy_model_vdj_gene_analysis/extract_relevant_gene_names.py
+++ paired_model/BERT2BERT/sqlite3_data_for_analysis/heavy_model_vdj_gene_analysis/extract_relevant_gene_names.py
@@ -2,12 +2,13 @@ import pandas as pd
 
 # Load the CSV file
 #input_csv_path = '/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/heavy_model_vdj_gene_analysis/relevant_cols_spaces_heavy_vdj_genes_paired_oas_db_test_set_extraction_no_dupl.txt'  
-input_csv_path = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/umap_tsne_pca_heavy_light_models/extracted_seqs_light_model_test_set_no_dupl.txt"
+#input_csv_path = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/umap_tsne_pca_heavy_light_models/extracted_seqs_light_model_test_set_no_dupl.txt"
+input_csv_path = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/umap_tsne_pca_heavy_light_models/extracted_seqs_heavy_model_test_set_80000.txt"
 df = pd.read_csv(input_csv_path)
 
 # Extract the required columns and create new columns with fewer values
 # df['v_call_fewer_heavy'] = df['v_call_heavy'].str.split('-').str[0]
-# df['d_call_fewer_heavy'] = df['d_call_heavy'].str.split('-').str[0]
+df['d_call_fewer'] = df['d_call'].str.split('-').str[0]
 
 df['v_call_fewer'] = df['v_call'].str.split('-').str[0]
 #df['j_call_fewer'] = df['j_call'].str.split('-').str[0]
@@ -24,12 +25,12 @@ df['v_call_fewer_star'] = df['v_call'].str.split('*').str[0]
 #                 'sequence_alignment_heavy_sep_light']]
 
 # Select the columns to save
-output_df = df[['v_call_fewer', 'j_call_fewer', 
+output_df = df[['v_call_fewer', 'j_call_fewer', 'd_call_fewer',
                 'v_call_fewer_star', 'v_call', 'd_call', 'j_call', 
                 'sequence_alignment_aa']]
 
 # Save the result to a new CSV file
-output_csv_path = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/umap_tsne_pca_heavy_light_models/fewer_gene_names_extracted_seqs_light_model_test_set_no_dupl.txt"
+output_csv_path = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/umap_tsne_pca_heavy_light_models/fewer_genes_extracted_seqs_heavy_model_test_set_80000.txt"
 output_df.to_csv(output_csv_path, index=False)
 
 print("New CSV file saved successfully!")
