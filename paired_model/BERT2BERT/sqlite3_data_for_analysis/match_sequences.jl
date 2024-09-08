1dfd941af8b82828f81a04caa3b5367771d8a800
leaBroe <lea.broennimann@students.unibe.ch>
2024-09-08 14:40:33 +0200

    updated julia script

--- paired_model/BERT2BERT/sqlite3_data_for_analysis/match_sequences.jl
+++ paired_model/BERT2BERT/sqlite3_data_for_analysis/match_sequences.jl
@@ -13,7 +13,8 @@ function compare_ids(queries, db, output_file)
     for ln in eachline(db)
         # Extract ID with regex
         parts = split(ln, ',')  # Splitting based on comma
-        ID = strip(parts[end])  # Assuming the ID is the last part after the comma!
+        #ID = strip(parts[end])  # Assuming the ID is the last part after the comma!
+        ID = strip(parts[5])  # Extracting ID from the 5th column!! -> always check the column number in your dataset!
         # Search in queries
         if ID in queries
             println(output_file, ln)  # Writing to file instead of standard output
@@ -21,16 +22,16 @@ function compare_ids(queries, db, output_file)
     end
 end
 
-# Open output file for writing
-open("/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/species_subgroups_analysis/full_test_data_extraction_species_diseases.txt", "w") do output_file
-    # Run the actual functions with output redirection
-    open("/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/full_test_data/paired_full_seqs_sep_test_no_ids.txt") do query_file
-        queries = get_ids(query_file)
-        open("/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/species_subgroups_analysis/paired_oas_db_full_extraction.csv") do db_file
-            compare_ids(queries, db_file, output_file)
-        end
-    end
-end
+# # Open output file for writing
+# open("/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/heavy_vdj_genes_paired_oas_db_test_set_extraction.txt", "w") do output_file
+#     # Run the actual functions with output redirection
+#     open("/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/full_test_data/paired_full_seqs_sep_test_no_ids.txt") do query_file
+#         queries = get_ids(query_file)
+#         open("/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/heavy_vdj_genes_paired_oas_db_full_extraction.csv") do db_file
+#             compare_ids(queries, db_file, output_file)
+#         end
+#     end
+# end
 
 # # Open output file for writing
 # open("/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/B-cell_analysis/Btypes_full_paired_test_data.csv", "w") do output_file
@@ -42,3 +43,18 @@ end
 #         end
 #     end
 # end
+
+# LIGHT model MLM
+# /ibmm_data2/oas_database/paired_lea_tmp/light_model/data/train_test_val_datasets/light_all_seqs_test_no_ids.txt
+# Open output file for writing
+open("/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/umap_tsne_pca_heavy_light_models/extracted_seqs_light_model_test_set.txt", "w") do output_file
+    # Run the actual functions with output redirection
+    open("/ibmm_data2/oas_database/paired_lea_tmp/light_model/data/train_test_val_datasets/light_all_seqs_test_no_ids.txt") do query_file
+        queries = get_ids(query_file)
+        open("/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/umap_tsne_pca_heavy_light_models/full_extraction_light_unpaired_seqs.csv") do db_file
+            compare_ids(queries, db_file, output_file)
+        end
+    end
+end
+
+# HEAVY model MLM
