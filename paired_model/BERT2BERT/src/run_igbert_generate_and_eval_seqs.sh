55020129a154300423c30a4f73ccf50b1f393aaa
leaBroe <lea.broennimann@students.unibe.ch>
2024-09-08 21:08:00 +0200

    updates

--- paired_model/BERT2BERT/src/run_igbert_generate_and_eval_seqs.sh
+++ paired_model/BERT2BERT/src/run_igbert_generate_and_eval_seqs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-#SBATCH --gres=gpu:alphafold:1
-#SBATCH --job-name=h2l_no_adaps
-#SBATCH --output=/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/logs/igbert_mlm_small_%j.o
-#SBATCH --error=/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/logs/igbert_mlm_small_%j.e
+#SBATCH --gres=gpu:h100:1
+#SBATCH --job-name=igbert_mlm
+#SBATCH --output=/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/logs/igbert_mlm_FULL_%j.o
+#SBATCH --error=/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/logs/igbert_mlm_FULL_%j.e
 
 eval "$(conda shell.bash hook)"
 conda init bash
