2240185612dad72cad09423a69ff7d847f56dced
leaBroe <lea.broennimann@students.unibe.ch>
2024-09-09 15:45:53 +0200

    added plots for LDA

---
+++ paired_model/BERT2BERT/sqlite3_data_for_analysis/LDA/LDA_of_embeddings.py
@@ -0,0 +1,116 @@
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+import pandas as pd
+import torch
+import numpy as np
+from transformers import BertModel, BertTokenizer, RobertaModel, RobertaTokenizer, RobertaForMaskedLM
+import pandas as pd
+from adapters import init
+import umap
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+
+# Assuming you have already computed `embeddings` and `labels`
+def get_mlm_last_layer_embeddings(model, tokenizer, sequences, device):
+    embeddings = []
+    model.to(device)
+    model.eval()
+    with torch.no_grad():
+        for seq in sequences:
+            inputs = tokenizer(seq, return_tensors="pt", padding="max_length", truncation=True, max_length=512).to(device)
+            
+            # Ensure to request hidden states
+            outputs = model(**inputs, output_hidden_states=True)
+            
+            # Access the last hidden states from the hidden_states tuple
+            last_hidden_states = outputs.hidden_states[-1]  # Get the last layer's hidden states
+            
+            # Mean pooling across the sequence length dimension
+            mean_pooled_output = last_hidden_states.mean(dim=1).cpu().numpy()
+            embeddings.append(mean_pooled_output)
+    
+    return np.vstack(embeddings)  # Stack into a numpy array
+
+
+small_heavy_encoder = "/ibmm_data2/oas_database/paired_lea_tmp/heavy_model/src/redo_ch/FULL_config_4_smaller_model_run_lr5e-5_500epochs_max_seq_length_512/checkpoint-117674391"
+small_light_decoder =  "/ibmm_data2/oas_database/paired_lea_tmp/light_model/src/redo_ch/FULL_config_4_smaller_model_run_lr5e-5_500epochs_max_seq_length_512/checkpoint-56556520"
+
+model_name = "heavy_model"
+
+# Load a pre-trained BERT model and tokenizer
+#tokenizer = BertTokenizer.from_pretrained(small_heavy_encoder)
+#model = BertModel.from_pretrained(small_heavy_encoder)
+
+tokenizer = BertTokenizer.from_pretrained(small_heavy_encoder)
+model = RobertaForMaskedLM.from_pretrained(small_heavy_encoder)
+
+# FULL unpaired heavy test seqs
+# col names: v_call_fewer,j_call_fewer,d_call_fewer,v_call_fewer_star,v_call,d_call,j_call,sequence_alignment_aa
+test_file_path = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/umap_tsne_pca_heavy_light_models/fewer_genes_extracted_seqs_heavy_model_test_set_80000.txt"
+
+# Small unpaired heavy test seqs
+# col names: v_call_fewer,j_call_fewer,d_call_fewer,v_call_fewer_star,v_call,d_call,j_call,sequence_alignment_aa
+#test_file_path = "/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/sqlite3_data_for_analysis/umap_tsne_pca_heavy_light_models/fewer_genes_extracted_seqs_heavy_model_test_set_100.txt"
+
+filtered_df = pd.read_csv(test_file_path)
+
+
+target = 'j_call_fewer'
+plot_title_target = "J Gene Families"
+plot_save_prefix = "j_call_fewer"
+
+labels = filtered_df[f'{target}'].tolist()
+
+heavy_sequences = filtered_df['sequence_alignment_aa'].tolist()
+
+# Device configuration
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Get embeddings
+embeddings = get_mlm_last_layer_embeddings(model, tokenizer, heavy_sequences, device)
+#embeddings = get_mlm_last_layer_embeddings(model, tokenizer, light_sequences, device)
+print(embeddings)  # This will print the array of embeddings
+
+
+# Ensure the labels are encoded as numerical values (if they aren't already)
+# use LabelEncoder to encode target labels with value between 0 and n_classes-1. https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.LabelEncoder.html
+label_encoder = LabelEncoder()
+encoded_labels = label_encoder.fit_transform(labels)
+
+# Apply LDA (Number of components should be less than number of classes)
+lda = LDA(n_components=2)
+lda_result = lda.fit_transform(embeddings, encoded_labels)
+
+# Define colors and markers (same as PCA/t-SNE/UMAP plots)
+cmap = plt.get_cmap('tab20')
+colors = cmap(np.linspace(0, 1, 20))
+additional_cmap = plt.get_cmap('tab20b')
+additional_colors = additional_cmap(np.linspace(0, 1, 20))
+all_colors = np.vstack((colors, additional_colors))
+
+markers = ['o', 'v', 's', 'P', '*', 'X', 'D']  # List of markers
+
+# Plot LDA result
+fig, ax = plt.subplots(figsize=(10, 8))
+unique_labels = set(labels)
+for idx, label in enumerate(unique_labels):
+    indices = [i for i, l in enumerate(labels) if l == label]
+    color = all_colors[idx % len(all_colors)]
+    marker = markers[idx % len(markers)]
+    ax.scatter(lda_result[indices, 0], lda_result[indices, 1], label=label, alpha=0.7, color=color, marker=marker, s=10)
+
+# Set title and labels
+ax.set_title(f'Differentiation of {plot_title_target} via LDA of Last Layer Embeddings')
+ax.set_xlabel('LDA Component 1')
+ax.set_ylabel('LDA Component 2')
+
+# Adjust legend position
+box = ax.get_position()
+ax.set_position([box.x0, box.y0 + box.height * 0.3, box.width, box.height * 0.7])
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=6)
+
+# Display and save the plot
+plt.show()
+plt.savefig(f'/ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/analysis_plots/{model_name}/LDA/full_{plot_save_prefix}_mlm_heavy2light_LDA.png')
+
+
+
