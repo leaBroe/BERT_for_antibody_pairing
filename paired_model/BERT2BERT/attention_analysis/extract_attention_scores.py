ada7b1584a2375b20aaa793b61529e94a9882d21
leaBroe <lea.broennimann@students.unibe.ch>
2024-11-04 10:01:05 +0100

    updated attention script

--- paired_model/BERT2BERT/attention_analysis/extract_attention_scores.py
+++ paired_model/BERT2BERT/attention_analysis/extract_attention_scores.py
@@ -64,6 +64,9 @@ adapter_name="heavy2light_adapter"
 
 model, tokenizer, generation_config = initialize_model_and_tokenizer(model_path, tokenizer_path, adapter_path, generation_config_path, device, adapter_name)
 
+# get information about the model
+#print(model.config)
+
 
 # update generation config to return attentions (if needed)
 generation_config.output_attentions = True
@@ -80,6 +83,7 @@ def format_attention(attention, layers=None, heads=None):
         if heads:
             layer_attention = layer_attention[heads]
         squeezed.append(layer_attention)
+    print(squeezed[0].shape)
     # num_layers x num_heads x seq_len x seq_len
     return torch.stack(squeezed)
 
@@ -88,27 +92,32 @@ def format_attention(attention, layers=None, heads=None):
 input_text = "QSALTQPASVSGSPGQSITISCTGTSSDVGGYNYVSWYQQHPGKAPKLMIYDVSNRPSGVSNRFSGSKSGNTASLTISGLQAEDEADYYCSSYTSSSTLVFGGGTKLTVL"
 #input_text = "Q A G L T Q P P S V S K G L R Q T A T L T C T G N S N N V G N Q G A A W L Q Q H Q G H P P K L L S Y R N N N R P S G I S E R L S A S R S G N T A S L T I T G L Q P E D E A D Y Y C S A W D S S L S A W V F G G G T K L T V L"
 
-#inputs = tokenizer(input_text, return_tensors='pt') ##COME ERA
-inputs = tokenizer(input_text, return_tensors='pt').to(device)
-#attention_mask=inputs['attention_mask']
-model.to(device)
-attention_mask = inputs['attention_mask'].to(device)
-outputs = model.generate(inputs['input_ids'], output_attentions = True, return_dict_in_generate=True, attention_mask=attention_mask, generation_config=generation_config)
+# #inputs = tokenizer(input_text, return_tensors='pt') ##COME ERA
+# inputs = tokenizer(input_text, return_tensors='pt').to(device)
+# #attention_mask=inputs['attention_mask']
+# model.to(device)
+# attention_mask = inputs['attention_mask'].to(device)
+# outputs = model.generate(inputs['input_ids'], output_attentions = True, return_dict_in_generate=True, attention_mask=attention_mask, generation_config=generation_config)
 
-print(type(outputs))
-print(outputs.keys())
+# print(type(outputs))
+# print(outputs.keys())
 
-attention = outputs['decoder_attentions'][-1]
+# attention = outputs['decoder_attentions'][-1][0] #attention from the last layer of the decoder
 
-inputs=inputs['input_ids'][0] #tokens id in the tokenizer vocab, same len as the input_test before adding whitespace and special char
-tokens = tokenizer.convert_ids_to_tokens(inputs)  # Convert input ids to token strings
+# print(type(outputs['decoder_attentions'][-1]))
+# print(outputs['decoder_attentions'][-1])
 
-last_attentions=format_attention(attention, layers=[-1], heads=[-1]) #extract attention from last layer from last head (29,15)
+# inputs=inputs['input_ids'][0] #tokens id in the tokenizer vocab, same len as the input_test before adding whitespace and special char
+# tokens = tokenizer.convert_ids_to_tokens(inputs)  # Convert input ids to token strings
 
-print(last_attentions.shape)
+# last_head_attentions=attention[:, -1, :, :] #attention from the last head of the decoder
 
-for i in list(last_attentions[0][0]):
-    print(i)
+# #last_attentions=format_attention(attention, layers=[-1], heads=[-1]) #extract attention from last layer from last head (29,15)
+
+# print(last_head_attentions.shape)
+
+# for i in list(last_head_attentions[0][0]):
+#     print(i)
 
 # # Average across heads to get a single attention matrix
 # # Shape after mean: (seq_len, seq_len)
@@ -119,7 +128,61 @@ for i in list(last_attentions[0][0]):
 #     print(f"Token {i}: Attention scores to all other tokens -> {attention_scores}")
 
 
+def attention_score_to_cls_token_and_to_all(input_text,model,device):
+
+    ''' Retrieve attention from model outputs attentions (tuple(torch.FloatTensor), optional, returned when output_attentions=True
+    is passed or when config.output_attentions=True) — Tuple of torch.FloatTensor (one for each layer)
+    of shape (batch_size, num_heads, sequence_length, sequence_length).
+
+    Outputs:
+    df_all_vs_all: pandas.Dataframe containing attention score for all tokens versus all tokens
+    att_to_cls: pandas.Series with attention score of all tokens related to the CLS token
+    df_att_to_cls_exp: att_to_cls in pandas.Dataframe format with score in exponential format
+
+    '''
+    model = model.to(device)
+    tokenizer = BertTokenizer.from_pretrained('Rostlab/prot_bert_bfd')
+    #inputs = tokenizer(input_text, return_tensors='pt') ##COME ERA
+    inputs = tokenizer(input_text, return_tensors='pt').to(device)
+    attention_mask=inputs['attention_mask']
+    outputs = model(inputs['input_ids'],attention_mask) #len 30 as the model layers #outpus.attentions
+    attention = outputs[-1] #outpus has 2 dimensions, the second one are the attentions outputs.attentions
+    inputs=inputs['input_ids'][0] #tokens id in the tokenizer vocab, same len as the input_test before adding whitespace and special char
+    tokens = tokenizer.convert_ids_to_tokens(inputs)  # Convert input ids to token strings
+    last_attentions=format_attention(attention, layers=[-1], heads=[-1]) #extract attention from last layer from last head (29,15)
+    #(last_attentions[0][0]) #extracting from stacked list
+    att_score=[]
+    for i in list(last_attentions[0][0]): #extracting every list of attention
+        #att_score.append((i).detach().numpy()) ### COME ERA
+        att_score.append((i).detach().cpu().numpy())
+        
+    #len(att_score) # same len as number of tokens (len text + special char)
+    x = np.stack(att_score, axis=0 )
+    m = np.asmatrix(x) # attention score as matrix
+    names = [_ for _ in tokens]
+    df_all_vs_all = pd.DataFrame(m, index=names, columns=names) #attention score matrix all tokens vs all tokens
+    #df_all_vs_all # df with attentio score all tokens vs all tokens
+    att_to_cls = df_all_vs_all.loc['[CLS]'] #attention score only vs cls token
+    #att_to_cls
+    attention_to_cls_exp=[]
+
+    att_to_cls.apply(lambda x: float(x))
+    for i in att_to_cls:
+        attention_to_cls_exp.append(math.exp(float(i)))
+
+    token_att_num=tuple(zip(names,attention_to_cls_exp)) #pairs : token - a.score
+
+    df_att_to_cls_exp=pd.DataFrame(token_att_num,columns=['token','attention'])
+    #df_att_to_cls_exp['index'] = range(1, len(df_att_to_cls_exp) + 1) # starting count from 1 in the index
+    df_att_to_cls_exp = df_att_to_cls_exp[df_att_to_cls_exp.token != '[CLS]']
+    df_att_to_cls_exp = df_att_to_cls_exp[df_att_to_cls_exp.token != '[SEP]']
+    #df_att_to_cls_exp
+
+    torch.cuda.empty_cache()
+    gc.collect()
+    return df_all_vs_all,att_to_cls,df_att_to_cls_exp
 
 
+test = attention_score_to_cls_token_and_to_all(input_text,model,device)
 
-
+print(test[0])
