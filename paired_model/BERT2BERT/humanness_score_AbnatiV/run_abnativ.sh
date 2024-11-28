49542320ec1968f51249230cf41e09113812ebd3
leaBroe <lea.broennimann@students.unibe.ch>
2024-11-28 11:14:32 +0100

    ANARCI is not working for own sequences in abnatv pipeline

--- paired_model/BERT2BERT/humanness_score_AbnatiV/run_abnativ.sh
+++ paired_model/BERT2BERT/humanness_score_AbnatiV/run_abnativ.sh
@@ -22,11 +22,25 @@
 #   -odir /ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/humanness_score_AbnatiV/PLAbDab_human_healthy_full_diverse_beam_search_5_temp_0.2_max_length_150_early_stopping_true_batch_size_64_epochs_50_lr_0.0001_wd_0.1/abnativ_output \
 #   -oid igk_true_sequences
 
-# Use conda run to execute the command within the 'abnativ' environment
-conda run -n abnativ abnativ score \
-  -nat VKappa \
-  -align \
-  -i /ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/humanness_score_AbnatiV/PLAbDab_human_healthy_full_diverse_beam_search_5_temp_0.2_max_length_150_early_stopping_true_batch_size_64_epochs_50_lr_0.0001_wd_0.1/igk_true_sequences_small.fasta \
-  -odir /ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/humanness_score_AbnatiV/PLAbDab_human_healthy_full_diverse_beam_search_5_temp_0.2_max_length_150_early_stopping_true_batch_size_64_epochs_50_lr_0.0001_wd_0.1/abnativ_output \
-  -oid igk_true_sequences
-  
+# IMPORTANT: run this in the terminal: export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH" before executing this scrpt to avoid the error: libstdc++.so.6: version `GLIBCXX_3.4.26' not found
+# IMPORTANT: run abnativ update to download the pretrained models if not already done
+
+# # Use conda run to execute the command within the 'abnativ' environment
+# conda run -n abnativ abnativ score \
+#   -nat VKappa \
+#   -align \
+#   -plot \
+#   -i /ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/humanness_score_AbnatiV/PLAbDab_human_healthy_full_diverse_beam_search_5_temp_0.2_max_length_150_early_stopping_true_batch_size_64_epochs_50_lr_0.0001_wd_0.1/igk_true_sequences_small.fasta \
+#   -odir /ibmm_data2/oas_database/paired_lea_tmp/paired_model/BERT2BERT/humanness_score_AbnatiV/PLAbDab_human_healthy_full_diverse_beam_search_5_temp_0.2_max_length_150_early_stopping_true_batch_size_64_epochs_50_lr_0.0001_wd_0.1/abnativ_output \
+#   -oid small_igk_true_sequences
+
+# kappa light variable seq from their own validation dataset, works
+#conda run -n abnativ abnativ score -nat VKappa -i EIVLTQSPATLSLSPGERATLSCRASQSVSIYVVWYQQKPGQAPRLLIYDASNRATGTPARFSGSGSGTDFTLTISSLEPEDGAVYYCQQRQRWPLTFGGGTRVEIK -odir test/test_results_kappa -oid test_single_kappa -align -plot
+#conda run -n abnativ abnativ score -nat VKappa -i EIVLTQSPATLSLSPGERATLSCRAS--QSVS------IYVVWYQQKPGQAPRLLIYD--------ASNRATGTPARFSGSGSG--TDFTLTISSLEPEDGAVYYCQQRQR-----------------------WPLTFGGGTRVEIK- -odir test/test_results_kappa_no_align -oid test_single_kappa_no_align -plot
+
+# from own dataset
+#conda run -n abnativ abnativ score -nat VKappa -i EIVLTQSPDFQSVTPGERVTITCRASQSIGSSLHWYQQKPDQSPKLLIKYASQSISGVPSRLSGSGSGTDFTLTITSLEAEDAATYYCHQSSSFPLTFG -odir test/test_results_kappa_own -oid test_single_kappa_own -align -plot
+# arbitrarily added gaps, but with ths it works -> problem with ANARCI numbering of own light sequences
+#conda run -n abnativ abnativ score -nat VKappa -i EIVLTQSPDFQSVTPGERVTITCRAS--QSIG---------------SSLHWYQQKPDQSPKLLIKY--------ASQSISGVPSRLSGSGSGT--DFTLTITSLEAEDAATY-----------------------YCHQSSSFPLTFG -odir test/test_results_kappa_own_no_align -oid test_single_kappa_own_no_align -plot
+
+
